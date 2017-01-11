1e1565f6bb5978197d72a84865c67e6817e1e313
Aksel Lunde Aase <mraksel@hotmail.no>
2017-01-11 14:14:29 +0100

    "Fixed" --help option. Now prints to a visible console.
    The "--help" option will now print helpful output to the appropriate console window, and exit the program afterwards. There is a bug described in the code that leads to a cluttered console window. I'm not aware of any solutions to this except removing the "--help" option.
    
    Modified the usage output from the previous version to provide a explanation for the color parameter. Issue #7 seems like the most user-friendly way to select colors, and will be the next step.

--- TranslucentTB/main.cpp
+++ TranslucentTB/main.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
-//#include <iostream>
+#include <iostream>
+#include <string>
 
 const HINSTANCE hModule = LoadLibraryW(L"user32.dll");
 
@@ -89,28 +90,91 @@ void SetWindowBlur(HWND hWnd)
 
 void PrintHelp()
 {
-	/*
-	The following won't print anything unless we explicitly call AllocConsole() and reopen stdout to
-	point to the console, or change the project settings to target the console subsystem instead
-	of the windows subsystem. Both have to problem of displaying an unwanted console in an
-	otherwise silent program. I'm not sure what the best solution would be - the simplest might be
-	to just write a small usage.txt file to accompany the program.
-	*/
+	// BUG - 
+	// For some reason, when launching this program in cmd.exe, it won't properly "notice"
+	// when the program has exited, and will not automatically write a new prompt to the console.
+	// Instead of printing the current directory as usual before it waits for a new command,
+	// it doesn't print anything, leading to a cluttered console.
+	// It's even worse in Powershell, where it actually WILL print the PS prompt before waiting for 
+	// a new command, but it does so on the line after "./TranslucentTB.exe --help", overwriting the 
+	// first line of output from this function, and gradually overwriting the following lines as you
+	// press enter. The PS shell just doesn't notice that anything gets printed to the console, and
+	// therefore it prints the PS prompt over this output instead of after. I don't know of any 
+	// solution to this, but I expect that setting the project type to SUBSYSTEM:CONSOLE would solve
+	// those issues. Again - I think a help file would be the best solution, so I'll do that in my 
+	// next commit.
 
-	/*
-	using namespace std;
-	wcout << "TranslucentTB by /u/IronManMark20" << endl;
-	wcout << "This program modifies the apperance of the windows taskbar" << endl;
-	wcout << "You can modify its behaviour by using the following parameters when launching the program:" << endl;
-	wcout << "  --blur        | will make the taskbar a blurry overlay of the background (default)." << endl;
-	wcout << "  --opaque      | will make the taskbar a solid color specified by the tint parameter." << endl;
-	wcout << "  --transparent | will make the taskbar a transparent color specified by the tint parameter. " << endl;
-	wcout << "                  the value of the alpha channel determines the opacity of the taskbar." << endl;
-	wcout << "  --tint COLOR  | specifies the color applied to the taskbar. COLOR is a hexadecimal 32 bit number." << endl;
-	wcout << "                  Will not affect the blur mode. By choosing a COLOR of zero in combination with --transparent" << endl;
-	wcout << "                  the taskbar becomes opaque and uses the selected system color scheme." << endl;
-	wcout << "  --help        | Displays this help message." << endl;
-	*/
+	BOOL hasconsole = true;
+	BOOL createdconsole = false;
+	// Try to attach to the parent console,
+	// allocate a new one if that isn't successful
+	if (!AttachConsole(ATTACH_PARENT_PROCESS))
+	{
+		if (!AllocConsole())
+		{
+			hasconsole = false;
+		}
+		else
+		{
+			createdconsole = true;
+		}
+	}
+
+	if (hasconsole)
+	{
+		FILE* outstream;
+		FILE* instream;
+		freopen_s(&outstream, "CONOUT$", "w", stdout);
+		freopen_s(&instream, "CONIN$", "w", stdin);
+
+		if (outstream)
+		{
+			using namespace std;
+			cout << endl;
+			cout << "TranslucentTB by /u/IronManMark20" << endl;
+			cout << "This program modifies the apperance of the windows taskbar" << endl;
+			cout << "You can modify its behaviour by using the following parameters when launching the program:" << endl;
+			cout << "  --blur        | will make the taskbar a blurry overlay of the background (default)." << endl;
+			cout << "  --opaque      | will make the taskbar a solid color specified by the tint parameter." << endl;
+			cout << "  --transparent | will make the taskbar a transparent color specified by the tint parameter. " << endl;
+			cout << "                  the value of the alpha channel determines the opacity of the taskbar." << endl;
+			cout << "  --tint COLOR  | specifies the color applied to the taskbar. COLOR is 32 bit number in hex format," << endl;
+			cout << "                  see explanation below. This will not affect the blur mode. If COLOR is zero in" << endl;
+			cout << "                  combination with --transparent the taskbar becomes opaque and uses the selected" << endl;
+			cout << "                  system color scheme." << endl;
+			cout << "  --help        | Displays this help message." << endl;
+			cout << endl;
+
+			cout << "Color format:" << endl;
+			cout << "  The parameter is interpreted as a three or four byte long number in hexadecimal format that" << endl;
+			cout << "  describes the four color channels ([alpha,] red, green and blue). These look like this:" << endl;
+			cout << "  0x80fe10a4 (the '0x' is optional). You often find colors in this format in the context of HTML and" << endl;
+			cout << "  web design, and there are many online tools to convert from familiar names to this format. These" << endl;
+			cout << "  tools might give you numbers starting with '#', in that case you can just remove the leading '#'." << endl;
+			cout << "  You should be able to find online tools by searching for \"color to hex\" or something similar." << endl;
+			cout << "  If the converter doesn't include alpha values (opacity), you can append them yourself at the start" << endl;
+			cout << "  of the number. Just convert a value between 0 and 255 to its hexadecimal value before you append it." << endl;
+			cout << endl;
+
+			if (createdconsole && instream)
+			{	
+				string wait;
+				
+				cout << "Press enter to exit the program." << endl;
+				if (!getline(cin, wait))
+				{
+					// Couldn't wait for user input, make the user close
+					// the program themselves so they can see the output.
+					cout << "Press Ctrl + C, Alt + F4, or click the close button to exit the program." << endl;
+					Sleep(INFINITE);
+				}
+
+				FreeConsole();
+			}
+
+			fclose(outstream);
+		}
+	}
 }
 
 void ParseOptions()
@@ -131,6 +195,7 @@ void ParseOptions()
 		if (wcscmp(arg, L"--help") == 0)
 		{
 			PrintHelp();
+			exit(0);
 		}
 		else if (wcscmp(arg, L"--blur") == 0)
 		{
