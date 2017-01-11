14e678a1c064baec25a322282e6785bda1092532
Aksel Lunde Aase <mraksel@hotmail.no>
2017-01-11 09:07:50 +0100

    Unicodeifying and whitespace fixing

--- TranslucentTB/main.cpp
+++ TranslucentTB/main.cpp
@@ -1,6 +1,6 @@
 #include <windows.h>
 
-const HINSTANCE hModule = LoadLibrary(TEXT("user32.dll"));
+const HINSTANCE hModule = LoadLibrary(L"user32.dll");
 
 struct ACCENTPOLICY
 {
@@ -23,16 +23,15 @@ const pSetWindowCompositionAttribute SetWindowCompositionAttribute = (pSetWindow
 
 void SetWindowBlur(HWND hWnd)
 {
-	
 	if (hModule)
-	{	
+	{
 		if (SetWindowCompositionAttribute)
 		{
 			ACCENTPOLICY policy = { 3, 0, 0, 0 }; // ACCENT_ENABLE_BLURBEHIND=3, ACCENT_INVALID=4...
 			WINCOMPATTRDATA data = { 19, &policy, sizeof(ACCENTPOLICY) }; // WCA_ACCENT_POLICY=19
 			SetWindowCompositionAttribute(hWnd, &data);
 		}
-		
+
 	}
 }
 
@@ -40,8 +39,9 @@ void SetWindowBlur(HWND hWnd)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
 {
 
-	HWND taskbar = FindWindowA("Shell_TrayWnd", NULL);
-	while (true) {
+	HWND taskbar = FindWindowW(L"Shell_TrayWnd", NULL);
+	while (true)
+	{
 		SetWindowBlur(taskbar); Sleep((DWORD)10);
 	}
 	FreeLibrary(hModule);
