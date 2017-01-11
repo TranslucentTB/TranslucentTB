e9cbfc6a46475dc473ed64f219c79cd639a8c552
olliethepikachu <olliethepikachu@icloud.com>
2017-01-11 17:06:18 +0800

    Added two window size options
    Added option for transparency when any window is maximised and
    transparency when the active (foreground) window is maximised. With any
    window maximised there is an (approximately) 0.1% CPU footprint on my
    SP4.

--- TranslucentTB/main.cpp
+++ TranslucentTB/main.cpp
@@ -23,10 +23,10 @@ struct WINCOMPATTRDATA
 };
 
 
-
 typedef BOOL(WINAPI*pSetWindowCompositionAttribute)(HWND, WINCOMPATTRDATA*);
 const pSetWindowCompositionAttribute SetWindowCompositionAttribute = (pSetWindowCompositionAttribute)GetProcAddress(hModule, "SetWindowCompositionAttribute");
 
+BOOL transparent, tpWhenMax, tpWhenFgMax;
 
 void SetWindowBlur(HWND hWnd, BOOL transparent)
 {
@@ -49,15 +49,30 @@ void SetWindowBlur(HWND hWnd, BOOL transparent)
 	}
 }
 
+BOOL CALLBACK EnumWindowsProcess(HWND hWnd, LPARAM lParam) {
+
+	WINDOWPLACEMENT result = {};
+	char str[256];
+	result.length = sizeof(WINDOWPLACEMENT);
+
+	::GetWindowPlacement(hWnd, &result);
+	if(result.showCmd == 3) { 
+		transparent = false; 
+	}
+
+	return true;
+
+}
+
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
 {
-
 	HWND mainTb, otherTb, fgWin;
-	BOOL transparent, tpWhenMax;
-	char str[256];
 	WINDOWPLACEMENT result = {};
 	result.length = sizeof(WINDOWPLACEMENT);
+	char str[256];
+
+	tpWhenMax = true;
 	
 	std::vector<HWND> secondTbVec;
 
@@ -71,28 +86,37 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int 
 	OutputDebugString(str);
 
 	
-	ifstream tp("transparent.xml");
-	if (!tp) { transparent = false; }
-	else { transparent = true; }
+	//ifstream tp("transparent.xml");
+	//if (!tp) { transparent = false; }
+	//else { transparent = true; }
 
-	ifstream tpWM("transparentWhenMaximised.xml");
-	if(!tpWM) { tpWhenMax = false; }
-	else { tpWhenMax = true; }
+	//ifstream tpWM("transparentWhenMaximised.xml");
+	//if(!tpWM) { tpWhenMax = false; }
+	//else { tpWhenMax = true; }
+	sprintf_s(str, "Heeey it worked! Reading from settings.ini");
+	OutputDebugString(str);
 	
 	while (true) {
-
 		secondTbVec.clear();
 		while (otherTb = FindWindowEx(0, otherTb, "Shell_SecondaryTrayWnd", ""))
 			secondTbVec.push_back(otherTb);
 
 		mainTb = FindWindowA("Shell_TrayWnd", NULL);
-		for( int a = 0; a < 10; a++) {
-			for( int a = 0; a < 50; a++) {
-				if(tpWhenMax)
+		for( int a = 0; a < 5; a++) {
+			for( int a = 0; a < 100; a++) {
+				if(tpWhenMax) {
+					transparent = true;
+					EnumWindows(EnumWindowsProcess, NULL); 
+				}
+					// if(result.showCmd == 3) { transparent = false; }
+					// else { transparent = true; }
+				if(tpWhenFgMax) {
 					fgWin = GetForegroundWindow();
 					::GetWindowPlacement(fgWin, &result);
 					if(result.showCmd == 3) { transparent = false; }
-					else { transparent = true; }
+					else {transparent = true;}
+				}
+
 				SetWindowBlur(mainTb, transparent); 
 				for(unsigned int i = 0; i < secondTbVec.size(); ++i) {
 					SetWindowBlur(secondTbVec[i], transparent); //call the virtual function
