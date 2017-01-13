649c09b7287f0ad9e912c419a9d8b5883fff1af4
olliethepikachu <olliethepikachu@icloud.com>
2017-01-13 09:20:18 +0800

    commited temp things

--- TranslucentTB/main.cpp
+++ TranslucentTB/main.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -27,6 +28,7 @@ typedef BOOL(WINAPI*pSetWindowCompositionAttribute)(HWND, WINCOMPATTRDATA*);
 const pSetWindowCompositionAttribute SetWindowCompositionAttribute = (pSetWindowCompositionAttribute)GetProcAddress(hModule, "SetWindowCompositionAttribute");
 
 BOOL transparent, tpWhenMax, tpWhenFgMax;
+std::vector<BOOL> transparentVec;
 char str[256];
 
 void SetWindowBlur(HWND hWnd, BOOL transparent)
@@ -50,6 +52,7 @@ void SetWindowBlur(HWND hWnd, BOOL transparent)
 	}
 }
 
+
 BOOL CALLBACK EnumWindowsProcess(HWND hWnd, LPARAM lParam) {
 
 	WINDOWPLACEMENT result = {};
@@ -68,26 +71,42 @@ BOOL CALLBACK EnumWindowsProcess(HWND hWnd, LPARAM lParam) {
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
 {
 	HWND mainTb, otherTb, fgWin;
+	unsigned int monitors = 1;
 	WINDOWPLACEMENT result = {};
+	HMONITOR _monitor;
+	MONITORINFOEX hMonInfo = {sizeof(MONITORINFOEX)};
 	result.length = sizeof(WINDOWPLACEMENT);
 	std::vector<HWND> secondTbVec;
 	tpWhenMax = true;
 	
 	fgWin = GetForegroundWindow();
 	::GetWindowPlacement(fgWin, &result);
-	sprintf_s(str, "State: %d !", result.showCmd);
-	OutputDebugString(str);
-	sprintf_s(str, "Heeey it worked! Reading from settings.ini");
+	
+	sprintf_s(str, "StartMonitors: %d !", monitors);
 	OutputDebugString(str);
 	
 	while (true) {
-		secondTbVec.clear();
-		while (otherTb = FindWindowEx(0, otherTb, "Shell_SecondaryTrayWnd", ""))
-			secondTbVec.push_back(otherTb);
-
+		monitors = 1;
+		secondTbVec.clear();	
+		transparentVec.clear();	
 		mainTb = FindWindowA("Shell_TrayWnd", NULL);
+		while (otherTb = FindWindowEx(0, otherTb, "Shell_SecondaryTrayWnd", ""))
+			monitors++;
+			secondTbVec.push_back(otherTb);
+			
+		if GetMonitorInfo(monitors[0], &hMonInfo) {		
+			sprintf_s(str, "EndMonitors: %d ", hMonInfo.szDevice);
+			OutputDebugString(str);
+		}
+
+		transparentVec.resize(monitors);
+		transparentVec[0] = transparent;
+		for(unsigned int i = 0; i < secondTbVec.size(); ++i) {
+			_monitor = MonitorFromWindow(secondTbVec[i], MONITOR_DEFAULTTONEAREST);
+		}
+
 		for( int a = 0; a < 5; a++) {
-			for( int a = 0; a < 100; a++) {
+			for( int b = 0; b < 100; b++) {
 				if(tpWhenMax) {
 					transparent = true;
 					EnumWindows(EnumWindowsProcess, NULL); 
