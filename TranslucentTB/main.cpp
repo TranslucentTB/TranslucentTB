c6bc43ac3b576d701ccac30191fd8e414d0139ed
olliethepikachu <olliethepikachu@icloud.com>
2017-01-15 13:01:45 +0800

    Added support for Dynamic Window States.

--- TranslucentTB/main.cpp
+++ TranslucentTB/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
+#include <map>
 
 //used for the tray things
 #include <shellapi.h>
@@ -48,25 +48,45 @@ struct OPTIONS
 {
 	int taskbar_appearance;
 	int color;
+	bool dynamic;
 } opt;
 
+enum TASKBARSTATE { Normal, WindowMaximised, StartMenuOpen }; // Create a state to store all 
+															  // states of the Taskbar
+			// Normal          | Proceed as normal. If no dynamic options are set, act as it says in opt.taskbar_appearance
+			// WindowMaximised | There is a window which is maximised on the monitor this HWND is in. Display as blurred.
+			// StartMenuOpen   | The Start Menu is open on the monitor this HWND is in. Display as it would be without TranslucentTB active.
+
+struct TASKBARPROPERTIES
+{
+	HMONITOR hmon;
+	TASKBARSTATE state;
+};
+
+int counter = 0;
 const int ACCENT_ENABLE_GRADIENT = 1; // Makes the taskbar a solid color specified by nColor. This mode doesn't care about the alpha channel.
 const int ACCENT_ENABLE_TRANSPARENTGRADIENT = 2; // Makes the taskbar a tinted transparent overlay. nColor is the tint color, sending nothing results in it interpreted as 0x00000000 (totally transparent, blends in with desktop)
 const int ACCENT_ENABLE_BLURBEHIND = 3; // Makes the taskbar a tinted blurry overlay. nColor is same as above.
 unsigned int WM_TASKBARCREATED;
-std::vector<HWND> taskbars; // Create a vector for all taskbars
+std::map<HWND, TASKBARPROPERTIES> taskbars; // Create a map for all taskbars
 
 
 typedef BOOL(WINAPI*pSetWindowCompositionAttribute)(HWND, WINCOMPATTRDATA*);
 static pSetWindowCompositionAttribute SetWindowCompositionAttribute = (pSetWindowCompositionAttribute)GetProcAddress(GetModuleHandle(TEXT("user32.dll")), "SetWindowCompositionAttribute");
 
-void SetWindowBlur(HWND hWnd)
+void SetWindowBlur(HWND hWnd, int appearance = 0) // `appearance` can be 0, which means 'follow opt.taskbar_appearance'
 {
 	if (SetWindowCompositionAttribute)
 	{
 		ACCENTPOLICY policy;
 
-		policy = { opt.taskbar_appearance, 2, opt.color, 0 };
+		if (appearance) // Custom taskbar appearance is set
+		{
+			policy = { appearance, 2, opt.color, 0 };
+		} else { // Use the defaults
+			policy = { opt.taskbar_appearance, 2, opt.color, 0 };
+		}
+		
 
 		WINCOMPATTRDATA data = { 19, &policy, sizeof(ACCENTPOLICY) }; // WCA_ACCENT_POLICY=19
 		SetWindowCompositionAttribute(hWnd, &data);
@@ -141,6 +161,8 @@ void PrintHelp()
 			cout << "                  see explanation below. This will not affect the blur mode. If COLOR is zero in" << endl;
 			cout << "                  combination with --transparent the taskbar becomes opaque and uses the selected" << endl;
 			cout << "                  system color scheme." << endl;
+			cout << "  --dynamic-ws     | will make the taskbar transparent when no windows are maximised in the current" << endl;
+			cout << "                  monitor, otherwise blurry." << endl;
 			cout << "  --help        | Displays this help message." << endl;
 			cout << endl;
 
@@ -187,6 +209,15 @@ void ParseSingleConfigOption(std::wstring arg, std::wstring value)
 		else if (value == L"transparent" ||
 				 value == L"translucent")
 			opt.taskbar_appearance = ACCENT_ENABLE_TRANSPARENTGRADIENT;
+	}
+	else if (arg == L"dynamic-ws")
+	{
+		if (value == L"true" ||
+			value == L"enable")
+			{
+				opt.taskbar_appearance = ACCENT_ENABLE_BLURBEHIND;
+				opt.dynamic = true;
+			}
 	}
 	else if (arg == L"color" ||
 			 arg == L"tint")
@@ -263,6 +294,11 @@ void SaveConfigFile()
 		else if (opt.taskbar_appearance == ACCENT_ENABLE_BLURBEHIND)
 			configstream << L"accent=blur" << endl;
 
+		configstream << L"; Dynamic states: Window States and (WIP) Start Menu";
+
+		if (opt.dynamic)
+			configstream << L"dynamic-ws=enable" << endl;
+
 		configstream << endl;
 		configstream << L"; Color and opacity of the taskbar." << endl;
 
@@ -301,6 +337,11 @@ void ParseSingleOption(std::wstring arg, std::wstring value)
 	{
 		opt.taskbar_appearance = ACCENT_ENABLE_TRANSPARENTGRADIENT;
 	}
+	else if (arg == L"--dynamic")
+		{
+			opt.taskbar_appearance = ACCENT_ENABLE_TRANSPARENTGRADIENT;
+			opt.dynamic = true;
+		}
 	else if (arg == L"--tint")
 	{
 		// The next argument should be a color in hex format
@@ -387,10 +428,22 @@ void ParseCmdOptions()
 
 void RefreshHandles()
 {
+	HWND _taskbar;
+	HMONITOR _monitor;
+	TASKBARPROPERTIES _properties;
+
 	taskbars.clear();
-	taskbars.push_back(FindWindowW(L"Shell_TrayWnd", NULL));
+	_taskbar = FindWindowW(L"Shell_TrayWnd", NULL);
+
+	_properties.hmon = MonitorFromWindow(_taskbar, MONITOR_DEFAULTTOPRIMARY);
+	_properties.state = Normal;
+	taskbars.insert(std::make_pair(_taskbar, _properties));
 	while (secondtaskbar = FindWindowEx(0, secondtaskbar, L"Shell_SecondaryTrayWnd", NULL))
-			taskbars.push_back(secondtaskbar); // We find all taskbars on second monitors and add them to a vector
+	{
+		_properties.hmon = MonitorFromWindow(secondtaskbar, MONITOR_DEFAULTTOPRIMARY);
+		_properties.state = Normal;
+		taskbars.insert(std::make_pair(secondtaskbar, _properties));
+	}
 }
 
 #pragma endregion
@@ -441,6 +494,55 @@ LRESULT CALLBACK TBPROCWND(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam
 	return DefWindowProc(hWnd, message, wParam, lParam);
 }
 
+BOOL CALLBACK EnumWindowsProcess(HWND hWnd, LPARAM lParam) 
+{
+	HMONITOR _monitor;
+
+	WINDOWPLACEMENT result = {};
+	::GetWindowPlacement(hWnd, &result);
+	if(result.showCmd == 3) { 
+		_monitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTOPRIMARY);
+		for (auto &taskbar: taskbars)
+		{
+			if (taskbar.second.hmon == _monitor)
+			{
+				taskbar.second.state = WindowMaximised;
+			}
+		}
+	}
+
+	return true;
+}
+
+void SetTaskbarBlur()
+{
+	if (opt.dynamic) {
+		if (counter >= 5)   // Change this if you want to change the time it takes for the program to update
+		{                   // 100 = 1 second; we use 5, because the difference is less noticeable and it has
+							// no large impact on CPU. We can change this if we feel that CPU is more important
+							// than response time.
+			counter = 0;
+			for (auto &taskbar: taskbars)
+			{
+				taskbar.second.state = Normal; // Reset taskbar state
+			}
+			EnumWindows(&EnumWindowsProcess, NULL);
+		}
+	}
+	
+	for (auto const &taskbar: taskbars)
+	{
+		if (taskbar.second.state == WindowMaximised)
+		{
+			SetWindowBlur(taskbar.first, ACCENT_ENABLE_BLURBEHIND);
+											// A window is maximised; let's make sure that we blur the window.
+		} else if (taskbar.second.state == Normal) {
+			SetWindowBlur(taskbar.first);  // Taskbar should be normal, call using normal transparency settings
+		}
+	} 
+	counter++;
+}
+
 NOTIFYICONDATA Tray;
 
 void initTray(HWND parent)
@@ -480,7 +582,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int 
 		// before the config file. Needs two passes on the command line arguments, lets leave that
 		// for later.
 		ParseCmdOptions(); //command line argument settings
-		ParseConfigFile(); //config file settings
+		ParseConfigFile(L"config.cfg"); //config file settings
 
 		MSG msg; // for message translation and dispatch
 		popup = LoadMenu(hInstance, MAKEINTRESOURCE(IDR_POPUP_MENU));
@@ -514,15 +616,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int 
 		}
 
 		RefreshHandles();
+		if (opt.dynamic)
+		{
+			EnumWindows(&EnumWindowsProcess, NULL); // Putting this here so there isn't a 
+			                                        // delay between when you start the 
+													// program and when the taskbar goes blurry
+		}
 		WM_TASKBARCREATED = RegisterWindowMessage(L"TaskbarCreated");
 		while (run) {
 			if (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE)) {
 				TranslateMessage(&msg);
 				DispatchMessage(&msg);
 			}
-			for(int i = 0; i > 10; i++) {
-				SetWindowBlur(taskbars[i]);
-			}
+			SetTaskbarBlur();
 			Sleep(10);
 		}
 		Shell_NotifyIcon(NIM_DELETE, &Tray);
