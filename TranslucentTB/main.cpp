9e9759d94bd5ba470b19c11b32d04741da14a26b
Charles Milette <charles.milette@gmail.com>
2017-05-17 13:20:49 -0400

    Do not change taskbar state if an instance is already running

--- TranslucentTB/main.cpp
+++ TranslucentTB/main.cpp
@@ -831,9 +831,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int 
 
 		if (shouldsaveconfig != DoNotSave)
 			SaveConfigFile();
+
+		opt.taskbar_appearance = ACCENT_ENABLE_GRADIENT;
+		SetTaskbarBlur();
 	}
-	opt.taskbar_appearance = ACCENT_ENABLE_GRADIENT;
-	SetTaskbarBlur();
 	CloseHandle(ev);
 	return 0;
 }
