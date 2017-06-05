1caca74e968ba888c11d80cf8a558b855818f4eb
Ethan Smith <ethan@ethanhs.me>
2017-06-05 12:57:07 -0700

    more docs improvements

--- TranslucentTB/main.cpp
+++ TranslucentTB/main.cpp
@@ -219,13 +219,15 @@ void PrintHelp()
 
 			cout << "Color format:" << endl;
 			cout << "  The parameter is interpreted as a three or four byte long number in hexadecimal format that" << endl;
-			cout << "  describes the four color channels ([alpha,] red, green and blue). These look like this:" << endl;
+			cout << "  describes the four color channels 0xAARRGGBB ([alpha,] red, green and blue). These look like this:" << endl;
 			cout << "  0x80fe10a4 (the '0x' is optional). You often find colors in this format in the context of HTML and" << endl;
 			cout << "  web design, and there are many online tools to convert from familiar names to this format. These" << endl;
 			cout << "  tools might give you numbers starting with '#', in that case you can just remove the leading '#'." << endl;
 			cout << "  You should be able to find online tools by searching for \"color to hex\" or something similar." << endl;
 			cout << "  If the converter doesn't include alpha values (opacity), you can append them yourself at the start" << endl;
 			cout << "  of the number. Just convert a value between 0 and 255 to its hexadecimal value before you append it." << endl;
+			cout << endl;
+			cout << "For examples, please see usage.md." << endl;
 			cout << endl;
 
 			if (createdconsole && instream)
