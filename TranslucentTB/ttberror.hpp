30f1d5a4a5e9ea3947fff11e8642e30993d1086c
Charles Milette <charles.milette@gmail.com>
2018-02-12 12:45:18 -0500

    Use a different buffer for logging messages then showing them

--- TranslucentTB/ttberror.hpp
+++ TranslucentTB/ttberror.hpp
@@ -14,64 +14,59 @@
 namespace Error {
 
 	enum class Level {
-		Fatal,
-		Error,
-		Log
+		Fatal,	// Show an error message to the user and immediatly exit
+		Error,	// Show an error message to the user and log to debug output
+		Log		// Log to debug output
 	};
 
 	bool Handle(const HRESULT error, const Level level, const std::wstring message)
 	{
 		if (FAILED(error))
 		{
-			std::wstring buffer;
-			buffer += message;
+			std::wstring boxbuffer;
+			if (level != Level::Log)
+			{
+				boxbuffer += message;
+				boxbuffer += L"\n\n";
 
-			if (level == Level::Log)
-			{
-				buffer += ' ';
-			}
-			else
-			{
-				buffer += L"\n\n";
-			}
+				if (level == Level::Fatal)
+				{
+					boxbuffer += L"Program will exit.\n\n";
+				}
 
-			if (level == Level::Fatal)
-			{
-				buffer += L"Program will exit.\n\n";
+				boxbuffer += L"Exception from HRESULT: ";
+				boxbuffer += _com_error(error).ErrorMessage();
 			}
 
-			buffer += L"Exception from HRESULT: ";
-			buffer += _com_error(error).ErrorMessage();
-
-			if (level == Level::Log)
+			std::wstring logbuffer;
+			if (level != Level::Fatal)
 			{
-				buffer += '\n';
+				logbuffer += message;
+				logbuffer += ' ';
+				logbuffer += L"Exception from HRESULT: ";
+				logbuffer += _com_error(error).ErrorMessage();
+				logbuffer += '\n';
 			}
 
 			switch (level)
 			{
 				case Level::Fatal:
 				{
-					MessageBox(NULL, buffer.c_str(), (std::wstring(App::NAME) + L" - Fatal error").c_str(), MB_ICONERROR | MB_OK | MB_SETFOREGROUND | MB_TOPMOST);
+					MessageBox(NULL, boxbuffer.c_str(), (std::wstring(App::NAME) + L" - Fatal error").c_str(), MB_ICONERROR | MB_OK | MB_SETFOREGROUND | MB_TOPMOST);
 					std::terminate();
 					break;
 				}
 
 				case Level::Error:
 				{
-					MessageBox(NULL, buffer.c_str(), (std::wstring(App::NAME) + L" - Error").c_str(), MB_ICONWARNING | MB_OK | MB_SETFOREGROUND);
+					MessageBox(NULL, boxbuffer.c_str(), (std::wstring(App::NAME) + L" - Error").c_str(), MB_ICONWARNING | MB_OK | MB_SETFOREGROUND);
 				}
 
 				case Level::Log:
 				{
-					OutputDebugString(buffer.c_str());
+					OutputDebugString(logbuffer.c_str());
 					break;
 				}
-			}
-
-			if (level == Level::Error)
-			{
-				OutputDebugString(L"\n");
 			}
 
 			return false;
