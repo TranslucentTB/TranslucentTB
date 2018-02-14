b9566c712e179a54ad1dff600401e42f9a35021c
Charles Milette <charles.milette@gmail.com>
2018-02-14 11:38:34 -0500

    Fix compiler warnings

--- TranslucentTB/ttberror.hpp
+++ TranslucentTB/ttberror.hpp
@@ -46,20 +46,14 @@ namespace Error {
 			logbuffer += _com_error(error).ErrorMessage();
 			Log::OutputMessage(logbuffer);
 
-			switch (level)
+			if (level == Level::Fatal)
 			{
-				case Level::Fatal:
-				{
-					MessageBox(NULL, boxbuffer.c_str(), (std::wstring(App::NAME) + L" - Fatal error").c_str(), MB_ICONERROR | MB_OK | MB_SETFOREGROUND | MB_TOPMOST);
-					std::terminate();
-					break;
-				}
-
-				case Level::Error:
-				{
-					MessageBox(NULL, boxbuffer.c_str(), (std::wstring(App::NAME) + L" - Error").c_str(), MB_ICONWARNING | MB_OK | MB_SETFOREGROUND);
-					break;
-				}
+				MessageBox(NULL, boxbuffer.c_str(), (std::wstring(App::NAME) + L" - Fatal error").c_str(), MB_ICONERROR | MB_OK | MB_SETFOREGROUND | MB_TOPMOST);
+				std::terminate();
+			}
+			else if (level == Level::Error)
+			{
+				MessageBox(NULL, boxbuffer.c_str(), (std::wstring(App::NAME) + L" - Error").c_str(), MB_ICONWARNING | MB_OK | MB_SETFOREGROUND);
 			}
 
 			return false;
