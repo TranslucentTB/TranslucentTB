80d2afe3bc9e2db00007486ef96a80ba84f2e6b3
Charles Milette <charles.milette@gmail.com>
2018-02-23 17:11:19 -0500

    Use a generic test certificate with TranslucentTB name, set AppX version in appveyor builds

--- update-version.ps1
+++ update-version.ps1
@@ -1,11 +1,13 @@
-﻿$content = (Get-Content TranslucentTB\TranslucentTB.rc)
-
-$tag = git describe --abbrev=0 --tags
+﻿$tag = git describe --abbrev=0 --tags
 $tag_hash = git show-ref -s $tag
 $commits = git rev-list $tag_hash`...HEAD --count
 $tags = (git tag | Measure-Object -Line).Lines
 
-$content = $content.Replace("1,0,0,1", "$tags,$commits,0,0")
-$content = $content.Replace("1.0.0.1", "$tag.$commits.$env:APPVEYOR_REPO_COMMIT.$env:APPVEYOR_REPO_BRANCH-$env:CONFIGURATION")
+$rc_content = (Get-Content TranslucentTB\TranslucentTB.rc)
+$rc_content = $rc_content.Replace("1,0,0,1", "$tags,$commits,0,0")
+$rc_content = $rc_content.Replace("1.0.0.1", "$tag.$commits.$env:APPVEYOR_REPO_COMMIT.$env:APPVEYOR_REPO_BRANCH-$env:CONFIGURATION")
+Set-Content TranslucentTB\TranslucentTB.rc -Value $rc_content
 
-Set-Content TranslucentTB\TranslucentTB.rc -Value $content
+$appx_content = (Get-Content StorePackage\Package.appxmanifest)
+$appx_content = $appx_content.Replace("1.0.0.0", "$tags.$commits.0.0")
+Set-Content StorePackage\Package.appxmanifest -Value $appx_content
