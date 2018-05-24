f9c8428d7a230da6c373b6ae9d6245f380509c51
Charles Milette <me@charlesmilette.net>
2018-05-24 08:27:56 -0400

    Cleanup SColour functions

--- CPicker/SColour.cpp
+++ CPicker/SColour.cpp
@@ -4,59 +4,52 @@
 // Updates the RGB color from the HSV
 void SColour::UpdateRGB()
 {
-	int conv;
-	double hue, sat, val;
-	int base;
+	const float val = v / 100.0f;
 
-	hue = (float)h / 100.0f;
-	sat = (float)s / 100.0f;
-	val = (float)v / 100.0f;
-
-	if ((float)s == 0) // Acromatic color (gray). Hue doesn't mind.
+	if (s == 0) // Acromatic color (gray). Hue doesn't mind.
 	{
-		conv = (unsigned short)(255.0f * val);
-		r = b = g = conv;
+		r = b = g = 255.0f * val;
 		return;
 	}
 
-	base = (unsigned short)(255.0f * (1.0 - sat) * val);
+	const float base = 255.0f * (1.0f - (s / 100.0f)) * val;
 
-	switch ((unsigned short)((float)h / 60.0f))
+	switch (h / 60)
 	{
 	case 0:
-		r = (unsigned short)(255.0f * val);
-		g = (unsigned short)((255.0f * val - base) * (h / 60.0f) + base);
+		r = 255.0f * val;
+		g = (255.0f * val - base) * (h / 60.0f) + base;
 		b = base;
 		break;
 
 	case 1:
-		r = (unsigned short)((255.0f * val - base) * (1.0f - ((h % 60) / 60.0f)) + base);
-		g = (unsigned short)(255.0f * val);
+		r = (255.0f * val - base) * (1.0f - ((h % 60) / 60.0f)) + base;
+		g = 255.0f * val;
 		b = base;
 		break;
 
 	case 2:
 		r = base;
-		g = (unsigned short)(255.0f * val);
-		b = (unsigned short)((255.0f * val - base) * ((h % 60) / 60.0f) + base);
+		g = 255.0f * val;
+		b = (255.0f * val - base) * ((h % 60) / 60.0f) + base;
 		break;
 
 	case 3:
 		r = base;
-		g = (unsigned short)((255.0f * val - base) * (1.0f - ((h % 60) / 60.0f)) + base);
-		b = (unsigned short)(255.0f * val);
+		g = (255.0f * val - base) * (1.0f - ((h % 60) / 60.0f)) + base;
+		b = 255.0f * val;
 		break;
 
 	case 4:
-		r = (unsigned short)((255.0f * val - base) * ((h % 60) / 60.0f) + base);
+		r = (255.0f * val - base) * ((h % 60) / 60.0f) + base;
 		g = base;
-		b = (unsigned short)(255.0f * val);
+		b = 255.0f * val;
 		break;
 
 	case 5:
-		r = (unsigned short)(255.0f * val);
+		r = 255.0f * val;
 		g = base;
-		b = (unsigned short)((255.0f * val - base) * (1.0f - ((h % 60) / 60.0f)) + base);
+		b = (255.0f * val - base) * (1.0f - ((h % 60) / 60.0f)) + base;
 		break;
 	}
 }
@@ -66,9 +59,7 @@ void SColour::UpdateHSV()
 {
 	const uint8_t &max = (std::max)((std::max)(r, g), b);
 	const uint8_t &min = (std::min)((std::min)(r, g), b);
-	const uint8_t delta = max - min;
-
-	short temp;
+	const float delta = max - min;
 
 	if (max == 0)
 	{
@@ -76,18 +67,29 @@ void SColour::UpdateHSV()
 		return;
 	}
 
-	v = max / 255.0 * 100.0;
-	s = ((double)delta / max) * 100.0;
+	v = max / 255.0f * 100.0f;
+	s = delta / (float)max * 100.0f;
 
+	short temp;
 	if (r == max)
-		temp = 60 * ((g - b) / (double)delta);
+	{
+		temp = 60 * ((g - b) / delta);
+	}
 	else if (g == max)
-		temp = 60 * (2.0 + (b - r) / (double)delta);
+	{
+		temp = 60 * (2 + (b - r) / delta);
+	}
 	else
-		temp = 60 * (4.0 + (r - g) / (double)delta);
+	{
+		temp = 60 * (4 + (r - g) / delta);
+	}
 
 	if (temp < 0)
+	{
 		h = temp + 360;
+	}
 	else
+	{
 		h = temp;
+	}
 }
