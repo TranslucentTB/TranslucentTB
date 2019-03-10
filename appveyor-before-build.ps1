a9c67f5ca43f6921a75704b6672df369ee1ccffa
Charles Milette <me@charlesmilette.net>
2019-03-10 00:12:52 -0500

    Another attempt at fixing CI

--- appveyor-before-build.ps1
+++ appveyor-before-build.ps1
@@ -1,17 +1,24 @@
+$ErrorActionPreference = "Stop"
+
 Set-Location C:\tools\vcpkg
-git pull
+
+# Git decides to use stderr for regular output apparently. PowerShell no likey.
+git pull 2>&1 | ForEach-Object { "$_" }
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+
 .\bootstrap-vcpkg.bat
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+if ($LastExitCode -ne 0) { exit $LastExitCode }
 
 .\vcpkg.exe integrate install
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+if ($LastExitCode -ne 0) { exit $LastExitCode }
 
 .\vcpkg.exe install detours:$env:CONFIGURATION-windows
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+if ($LastExitCode -ne 0) { exit $LastExitCode }
 
 .\vcpkg.exe install gtest:$env:CONFIGURATION-windows
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+if ($LastExitCode -ne 0) { exit $LastExitCode }
 
 Set-Location $env:APPVEYOR_BUILD_FOLDER
+
 .\update-version.ps1
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+if ($LastExitCode -ne 0) { exit $LastExitCode }
--- update-version.ps1
+++ update-version.ps1
@@ -5,10 +5,10 @@ $tag_hash = git show-ref -s $tag
 $commits = git rev-list $tag_hash`...HEAD --count
 $tags = (git tag | Measure-Object -Line).Lines
 
-$rc_content = (Get-Content TranslucentTB\TranslucentTB.rc2)
+$rc_content = (Get-Content TranslucentTB\resources\TranslucentTB.rc2)
 $rc_content = $rc_content.Replace("1,0,0,1", "$tags,$commits,0,0")
 $rc_content = $rc_content.Replace("1.0.0.1", "$tag.$commits.$env:APPVEYOR_REPO_COMMIT.$env:APPVEYOR_REPO_BRANCH-$env:CONFIGURATION")
-Set-Content TranslucentTB\TranslucentTB.rc2 -Value $rc_content
+Set-Content TranslucentTB\resources\TranslucentTB.rc2 -Value $rc_content
 
 $appx_content = (Get-Content AppPackage\Package.appxmanifest).Replace("1.0.0.0", "$tags.$commits.0.0")
 Set-Content AppPackage\Package.appxmanifest -Value $appx_content
