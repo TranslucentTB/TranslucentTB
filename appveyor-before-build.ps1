8f67006a8f4e9fd526ff8ed65421865d60f1426b
Charles Milette <me@charlesmilette.net>
2019-03-09 23:41:41 -0500

    woops

---
+++ appveyor-before-build.ps1
@@ -0,0 +1,19 @@
+$ErrorActionPreference = "Stop"
+
+Set-Location C:\tools\vcpkg
+git pull
+.\bootstrap-vcpkg.bat
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+
+.\vcpkg.exe integrate install
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+
+.\vcpkg.exe install detours:$env:CONFIGURATION-windows
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+
+.\vcpkg.exe install gtest:$env:CONFIGURATION-windows
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+
+Set-Location $env:APPVEYOR_BUILD_FOLDER
+.\update-version.ps1
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
