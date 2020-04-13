14e01a3455285ebd0917c9703e6eba50bb7e37f8
Charles Milette <charles.milette@gmail.com>
2020-04-13 02:50:32 -0400

    Drop handles if unused folder watcher

--- TranslucentTB/folderwatcher.hpp
+++ TranslucentTB/folderwatcher.hpp
@@ -49,10 +49,13 @@ class FolderWatcher {
 			that->rearm();
 			break;
 
-		default:
-			LastErrorHandle(spdlog::level::warn, L"Error occured while watching directory");
-			[[fallthrough]];
 		case ERROR_OPERATION_ABORTED:
+			break;
+
+		default:
+			that->m_FolderHandle.reset();
+			that->m_Buffer.reset();
+			LastErrorHandle(spdlog::level::warn, L"Error occured while watching directory");
 			break;
 		}
 	}
@@ -61,6 +64,8 @@ class FolderWatcher {
 	{
 		if (!ReadDirectoryChangesW(m_FolderHandle.get(), m_Buffer.get(), m_BufferSize, m_Recursive, m_Filter, nullptr, &m_Overlapped, callback))
 		{
+			m_FolderHandle.reset();
+			m_Buffer.reset();
 			LastErrorHandle(spdlog::level::warn, L"Failed to arm directory watcher");
 		}
 	}
@@ -94,6 +99,7 @@ public:
 			}
 			else
 			{
+				m_FolderHandle.reset();
 				LastErrorHandle(spdlog::level::warn, L"Failed to allocate overlapped IO buffer");
 			}
 		}
