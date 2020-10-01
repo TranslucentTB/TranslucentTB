c4b4e01fddab5b8ebe4743886e9a761520173ec3
Charles Milette <charles.milette@gmail.com>
2020-10-01 02:07:19 -0400

    I guess we need NuGet cppwinrt for the tests too now

--- Tests/util/color.cpp
+++ Tests/util/color.cpp
@@ -6,17 +6,17 @@
 
 using namespace testing;
 
-TEST(Color_Constructor, DefaultConstructorIsTransparentBlack)
+TEST(Util_Color_Constructor, DefaultConstructorIsTransparentBlack)
 {
 	ASSERT_EQ(Util::Color(), Util::Color(0x00, 0x00, 0x00, 0x00));
 }
 
-TEST(Color_Constructor, ConstructorFromRGBUsesFullAlpha)
+TEST(Util_Color_Constructor, ConstructorFromRGBUsesFullAlpha)
 {
 	ASSERT_EQ(Util::Color(0x00, 0x00, 0x00), Util::Color(0x00, 0x00, 0x00, 0xFF));
 }
 
-TEST(Color_Constructor, ConstructorFromRGBAGivesCorrectValue)
+TEST(Util_Color_Constructor, ConstructorFromRGBAGivesCorrectValue)
 {
 	const Util::Color col = { 0xDE, 0xAD, 0xBE, 0xEF };
 	ASSERT_EQ(col.R, 0xDE);
@@ -25,84 +25,84 @@ TEST(Color_Constructor, ConstructorFromRGBAGivesCorrectValue)
 	ASSERT_EQ(col.A, 0xEF);
 }
 
-TEST(Color_Constructor, ConstructorFromWinRTGivesCorrectValue)
+TEST(Util_Color_Constructor, ConstructorFromWinRTGivesCorrectValue)
 {
 	const winrt::Windows::UI::Color winrtCol = { .A = 0xEF, .R = 0xDE, .G = 0xAD, .B = 0xBE };
 	ASSERT_EQ(Util::Color(winrtCol), Util::Color(0xDE, 0xAD, 0xBE, 0xEF));
 }
 
-TEST(Color_Constructor, ConstructorFromUInt32GivesCorrectValue)
+TEST(Util_Color_Constructor, ConstructorFromUInt32GivesCorrectValue)
 {
 	ASSERT_EQ(Util::Color(0xDEADBEEF), Util::Color(0xDE, 0xAD, 0xBE, 0xEF));
 }
 
-TEST(Color_ToRGBA, ReturnsCorrectValue)
+TEST(Util_Color_ToRGBA, ReturnsCorrectValue)
 {
 	ASSERT_EQ(Util::Color(0xDE, 0xAD, 0xBE, 0xEF).ToRGBA(), 0xDEADBEEF);
 }
 
-TEST(Color_ToABGR, ReturnsCorrectValue)
+TEST(Util_Color_ToABGR, ReturnsCorrectValue)
 {
 	ASSERT_EQ(Util::Color(0xDE, 0xAD, 0xBE, 0xEF).ToABGR(), 0xEFBEADDE);
 }
 
-TEST(Color_ToString, ReturnsCorrectString)
+TEST(Util_Color_ToString, ReturnsCorrectString)
 {
 	fmt::wmemory_buffer buf;
 	Util::Color(0xDE, 0xAD, 0xBE, 0xEF).ToString(buf);
 	ASSERT_EQ(Util::ToStringView(buf), L"#DEADBEEF");
 }
 
-TEST(Color_ToString, PadsLeftRight)
+TEST(Util_Color_ToString, PadsLeftRight)
 {
 	fmt::wmemory_buffer buf;
 	Util::Color(0x00, 0x00, 0xFF, 0x00).ToString(buf);
 	ASSERT_EQ(Util::ToStringView(buf), L"#0000FF00");
 }
 
-TEST(Color_ToString, TransparentBlackColor)
+TEST(Util_Color_ToString, TransparentBlackColor)
 {
 	fmt::wmemory_buffer buf;
 	Util::Color(0x00, 0x00, 0x00, 0x00).ToString(buf);
 	ASSERT_EQ(Util::ToStringView(buf), L"#00000000");
 }
 
-TEST(Color_FromString, Parses3DigitColor)
+TEST(Util_Color_FromString, Parses3DigitColor)
 {
 	ASSERT_EQ(Util::Color::FromString(L"#FAF"), Util::Color(0xFF, 0xAA, 0xFF));
 }
 
-TEST(Color_FromString, Parses4DigitColor)
+TEST(Util_Color_FromString, Parses4DigitColor)
 {
 	ASSERT_EQ(Util::Color::FromString(L"#DEAD"), Util::Color(0xDD, 0xEE, 0xAA, 0xDD));
 }
 
-TEST(Color_FromString, Parses6DigitColor)
+TEST(Util_Color_FromString, Parses6DigitColor)
 {
 	ASSERT_EQ(Util::Color::FromString(L"#C0FFEE"), Util::Color(0xC0, 0xFF, 0xEE));
 }
 
-TEST(Color_FromString, Parses8DigitColor)
+TEST(Util_Color_FromString, Parses8DigitColor)
 {
 	ASSERT_EQ(Util::Color::FromString(L"#DEADBEEF"), Util::Color(0xDE, 0xAD, 0xBE, 0xEF));
 }
 
-TEST(Color_FromString, TrimsInput)
+TEST(Util_Color_FromString, TrimsInput)
 {
 	ASSERT_EQ(Util::Color::FromString(L"   #FFFFFF \t \n"), Util::Color(0xFF, 0xFF, 0xFF));
 }
 
-TEST(Color_FromString, ThrowsWhenColorDoesntStartsWithPrefix)
+TEST(Util_Color_FromString, ThrowsWhenColorDoesntStartsWithPrefix)
 {
 	ASSERT_THROW(Util::Color::FromString(L"FFFFFF"), std::invalid_argument);
 }
 
-TEST(Color_FromString, ThrowsWhenColorIsNotValidCharacterCount)
+TEST(Util_Color_FromString, ThrowsWhenColorIsNotValidCharacterCount)
 {
 	ASSERT_THROW(Util::Color::FromString(L"#FFFFFFF"), std::invalid_argument);
 }
 
-TEST(Color_ToWinRT, ConvertsToSameColor)
+TEST(Util_Color_ToWinRT, ConvertsToSameColor)
 {
 	const winrt::Windows::UI::Color convertedCol = Util::Color { 0xDE, 0xAD, 0xBE, 0xEF };
 	const winrt::Windows::UI::Color originalCol = { .A = 0xEF, .R = 0xDE, .G = 0xAD, .B = 0xBE };
@@ -110,12 +110,12 @@ TEST(Color_ToWinRT, ConvertsToSameColor)
 	ASSERT_EQ(convertedCol, originalCol);
 }
 
-TEST(Color_Equality, ReturnsTrueWhenSame)
+TEST(Util_Color_Equality, ReturnsTrueWhenSame)
 {
 	ASSERT_THAT(Util::Color(0xDE, 0xAD, 0xBE, 0xEF) == Util::Color(0xDE, 0xAD, 0xBE, 0xEF), IsTrue());
 }
 
-TEST(Color_Equality, ReturnsFalseWhenDifferent)
+TEST(Util_Color_Equality, ReturnsFalseWhenDifferent)
 {
 	ASSERT_THAT(Util::Color(0xDE, 0xAD, 0xBE, 0xEF) == Util::Color(0xC0, 0xFF, 0xEE, 0x00), IsFalse());
 }
