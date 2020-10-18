5c878946599015712207d3676265295a8e92cd7a
Charles Milette <charles.milette@gmail.com>
2020-10-18 01:50:36 -0400

    More x86 cleanup

--- Common/arch.h
+++ Common/arch.h
@@ -2,17 +2,9 @@
 
 // Those are defines required by various Windows headers to build.
 
-#if defined(_M_IX86)
-# ifndef _x86_
-#  define _X86_
-# endif
-#elif defined(_M_AMD64)
+#if defined(_M_AMD64)
 # ifndef _AMD64_
 #  define _AMD64_
-# endif
-#elif defined(_M_ARM)
-# ifndef _ARM_
-#  define _ARM_
 # endif
 #elif defined(_M_ARM64)
 # ifndef _ARM64_
