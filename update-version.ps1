2536d8b3828a5b79ad8c6091bfc59313dee91a55
Charles Milette <charles.milette@gmail.com>
2020-12-07 17:16:01 -0500

    Version support

--- update-version.ps1
+++ update-version.ps1
@@ -4,7 +4,7 @@ $tag = git describe --abbrev=0 --tags
 $tag_commas = $tag.Replace(".", ",")
 $tag_hash = git show-ref -s $tag
 $commits_since_tag = git rev-list $tag_hash`...HEAD --count
-$current_commit = $env:APPVEYOR_REPO_COMMIT.Substring(0, 7)
+$current_commit = $env:BUILD_SOURCEVERSION.Substring(0, 7)
 
 $exe_short_version = "$tag_commas,$commits_since_tag,0"
 $exe_full_version = "$tag.$commits_since_tag.$current_commit"
@@ -19,5 +19,3 @@ Set-Content $info_file -Value $info_content
 $appx_file = "AppPackage\Package.appxmanifest"
 $appx_content = (Get-Content $appx_file).Replace("1.0.0.0", $appx_short_version)
 Set-Content $appx_file -Value $appx_content
-
-Update-AppveyorBuild -Version $exe_full_version
