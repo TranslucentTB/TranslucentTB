61c8f3ecd37bdf5c40df98264b056aad90b4f61b
Charles Milette <charles.milette@gmail.com>
2021-02-26 12:37:52 -0500

    Fix build

--- vcpkg/triplets/arm64-windows-ttb.cmake
+++ vcpkg/triplets/arm64-windows-ttb.cmake
@@ -2,7 +2,7 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
-include("base.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/base.cmake")
 
 set(VCPKG_C_FLAGS ${BASE_C_FLAGS})
 set(VCPKG_CXX_FLAGS "${VCPKG_C_FLAGS} ${BASE_CXX_FLAGS}")
--- vcpkg/triplets/x64-windows-ttb.cmake
+++ vcpkg/triplets/x64-windows-ttb.cmake
@@ -2,7 +2,7 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
-include("base.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/base.cmake")
 
 set(VCPKG_C_FLAGS "${BASE_C_FLAGS} /QIntel-jcc-erratum /guard:ehcont")
 set(VCPKG_CXX_FLAGS "${VCPKG_C_FLAGS} ${BASE_CXX_FLAGS}")
