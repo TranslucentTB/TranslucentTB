6df0a3ea53ac69121842421fe5e62b85ecf85ae8
Charles Milette <charles.milette@gmail.com>
2021-04-21 17:03:32 -0400

    Force on classic IUnknown support for cppwinrt

--- Common/winrt.hpp
+++ Common/winrt.hpp
@@ -1,12 +1,7 @@
 #pragma once
-#ifndef WINRT_BASE_H
-# include <Unknwn.h>
-# include <winrt/base.h>
-#else
-# ifndef __IUnknown_INTERFACE_DEFINED__
-#  error "<winrt/base.h> has been previously included without COM support"
-# endif
-#endif
+#include <guiddef.h>
+#include <Unknwn.h>
+#include <winrt/base.h>
 
 namespace winrt::Windows::Foundation::Collections {}
 namespace winrt::Windows::UI::Xaml {}
