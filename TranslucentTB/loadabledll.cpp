444718977b45952c5ffb154d0ff3472e3ede87c1
Charles Milette <charles.milette@gmail.com>
2023-10-24 16:41:32 -0400

    Copy DLLs to %TEMP% even in portable mode

--- TranslucentTB/loadabledll.cpp
+++ TranslucentTB/loadabledll.cpp
@@ -10,37 +10,49 @@
 
 std::filesystem::path LoadableDll::GetDllPath(const std::optional<std::filesystem::path> &storageFolder, std::wstring_view dll)
 {
+	std::error_code err;
+
 	const auto [loc, hr] = win32::GetExeLocation();
 	HresultVerify(hr, spdlog::level::critical, L"Failed to determine executable location!");
 
 	std::filesystem::path dllPath = loc.parent_path() / dll;
 
+	// copy the file over to a place Explorer can read. It can't be injected from WindowsApps.
+	// when running portable, copy it to the temp folder anyways, to allow ejecting the device TTB is running from.
+	std::filesystem::path tempFolder;
 	if (storageFolder)
 	{
-		// copy the file over to a place Explorer can read. It can't be injected from WindowsApps.
-		auto tempDllPath = *storageFolder / L"TempState" / dll;
-
-		std::error_code err;
-		std::filesystem::copy_file(dllPath, tempDllPath, std::filesystem::copy_options::update_existing, err);
-		if (err)
-		{
-			if (err.category() == std::system_category() && err.value() == ERROR_SHARING_VIOLATION)
-			{
-				Localization::ShowLocalizedMessageBox(IDS_RESTART_REQUIRED, MB_OK | MB_ICONWARNING | MB_SETFOREGROUND).join();
-				ExitProcess(1);
-			}
-			else
-			{
-				StdErrorCodeHandle(err, spdlog::level::critical, std::format(L"Failed to copy {}", dll));
-			}
-		}
-
-		return tempDllPath;
+		tempFolder = *storageFolder / L"TempState";
 	}
 	else
 	{
-		return dllPath;
+		tempFolder = std::filesystem::temp_directory_path(err);
+		if (!err)
+		{
+			tempFolder /= APP_NAME;
+		}
+		else
+		{
+			StdErrorCodeHandle(err, spdlog::level::critical, L"Failed to get temp folder path.");
+		}
 	}
+
+	auto tempDllPath = tempFolder / dll;
+	std::filesystem::copy_file(dllPath, tempDllPath, std::filesystem::copy_options::update_existing, err);
+	if (err)
+	{
+		if (err.category() == std::system_category() && err.value() == ERROR_SHARING_VIOLATION)
+		{
+			Localization::ShowLocalizedMessageBox(IDS_RESTART_REQUIRED, MB_OK | MB_ICONWARNING | MB_SETFOREGROUND).join();
+			ExitProcess(1);
+		}
+		else
+		{
+			StdErrorCodeHandle(err, spdlog::level::critical, std::format(L"Failed to copy {}", dll));
+		}
+	}
+
+	return tempDllPath;
 }
 
 wil::unique_hmodule LoadableDll::LoadDll(const std::filesystem::path &location)
