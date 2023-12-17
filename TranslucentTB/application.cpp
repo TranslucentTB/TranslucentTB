117fa89a41e82d8f24e4553dac46e55821951efa
Charles Milette <charles.milette@gmail.com>
2023-12-17 01:49:56 -0500

    Update WinUI dyndep version

--- TranslucentTB/application.cpp
+++ TranslucentTB/application.cpp
@@ -90,10 +90,10 @@ Application::Application(HINSTANCE hInst, std::optional<std::filesystem::path> s
 		hInst,
 		L"Microsoft.UI.Xaml.2.8_8wekyb3d8bbwe",
 		PACKAGE_VERSION {
-			// 8.2305.5001.0 but the order is reversed because that's how the struct is.
+			// 8.2310.30001.0 but the order is reversed because that's how the struct is.
 			.Revision = 0,
-			.Build = 5001,
-			.Minor = 2305,
+			.Build = 30001,
+			.Minor = 2310,
 			.Major = 8
 		},
 		storageFolder.has_value()
