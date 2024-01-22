7a4039d3c939b615222d0d4d93f174f8d55756a5
Charles Milette <charles.milette@gmail.com>
2024-01-22 02:52:09 -0500

    Fix a couple things

--- .azp/update-version.ps1
+++ .azp/update-version.ps1
@@ -52,13 +52,13 @@ $head_short = $head_hash.Substring(0, 7)
 $short_version = "$tag.$commits_since_tag.0"
 $full_version = "$tag.$commits_since_tag.$head_short"
 
-$info_file = "Common\appinfo.hpp"
+$info_file = "$PSScriptRoot\..\Common\appinfo.hpp"
 $info_content = (Get-Content $info_file)
 $info_content = $info_content.Replace("1,0,0,1", $short_version.Replace(".", ","))
 $info_content = $info_content.Replace("1.0.0.1", $full_version)
 Set-Content $info_file -Value $info_content
 
-$appx_file = "AppPackage\Package.appxmanifest"
+$appx_file = "$PSScriptRoot\..\AppPackage\Package.appxmanifest"
 $appx_content = (Get-Content $appx_file).Replace("1.0.0.0", $short_version)
 Set-Content $appx_file -Value $appx_content
 
