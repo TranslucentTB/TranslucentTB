2ff44ba47716ccc2b0c155969e1d72a7c3b7db43
Charles Milette <charles.milette@gmail.com>
2024-02-13 01:34:17 -0500

    Avoid double-compressing portable build artifacts, sign the MSIX for CI builds

--- .azp/scripts/create-portable.ps1
+++ .azp/scripts/create-portable.ps1
@@ -3,27 +3,31 @@ param(
 	[string] $Configuration,
 	[string] $WorkDir,
 	[string] $OutputDir,
-	[switch] $NoConfig
+	[switch] $NoConfig,
+	[switch] $Compress
 )
 
 $ErrorActionPreference = "Stop"
 
-New-Item -Path $WorkDir -Name "portable-$Platform-$Configuration" -ItemType "Directory" -Force
+New-Item -Path "$WorkDir" -Name "portable-$Platform-$Configuration" -ItemType "Directory" -Force
+New-Item -Path "$OutputDir" -ItemType "Directory" -Force
 
-Copy-Item -Path "$PSScriptRoot\..\..\AppPackage\bin\$Platform\$Configuration\*" -Destination "$WorkDir\portable-$Platform-$Configuration" -Recurse -Include @("*.exe", "*.dll", "resources.pri", "Assets")
+Copy-Item -Path "$PSScriptRoot\..\..\AppPackage\bin\$Platform\$Configuration\*" -Destination $(if ($Compress) {"$WorkDir\portable-$Platform-$Configuration"} else {"$OutputDir"}) -Recurse -Include @("*.exe", "*.dll", "resources.pri", "Assets")
 
-$platform_lower = $Platform.ToLower()
-if ($NoConfig)
+if ($Compress)
 {
-	$filename = "TranslucentTB-portable-$platform_lower.zip"
-}
-else
-{
-	$configuration_lower = $Configuration.ToLower()
-	$filename = "TranslucentTB-portable-$platform_lower-$configuration_lower.zip"
-}
+	$platform_lower = $Platform.ToLower()
+	if ($NoConfig)
+	{
+		$filename = "TranslucentTB-portable-$platform_lower.zip"
+	}
+	else
+	{
+		$configuration_lower = $Configuration.ToLower()
+		$filename = "TranslucentTB-portable-$platform_lower-$configuration_lower.zip"
+	}
 
-New-Item -Path $OutputDir -ItemType "Directory" -Force
-Compress-Archive -Path "$WorkDir\portable-$Platform-$Configuration\*" -DestinationPath "$OutputDir\$filename"
+	Compress-Archive -Path "$WorkDir\portable-$Platform-$Configuration\*" -DestinationPath "$OutputDir\$filename"
+}
 
 Remove-Item -Path "$WorkDir\portable-$Platform-$Configuration" -Recurse
